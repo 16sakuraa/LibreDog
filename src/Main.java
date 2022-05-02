be85c5c9ec16c5d2d34d0a2ad4461ebbd82e7e50
16sakuraa <64010860@kmitl.ac.th>
2022-05-02 13:29:12 +0700

    UI Change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -80,14 +82,24 @@ public class Main extends Application {
 		});
 
 		
-		
-		engine.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+		engine.setUserAgent("Mozilla/5.0 (Linux; U; Android 4.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13");
+		//engine.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
 		
 		
 		VBox root = new VBox();
-		root.getChildren().addAll(myWebView, back, forward , reload , urlBar);
+
+		VBox.setMargin(urlBar , new Insets(10, 10, 10, 10));
+		VBox.setMargin(back , new Insets(0, 0, 10, 10));
+		VBox.setMargin(forward , new Insets(-35, 90, 10, 60));
+		VBox.setMargin(reload , new Insets(-35, 90, 10, 130));
 		
-		Scene scene = new Scene(root, 800, 500);
+
+		root.getChildren().addAll(urlBar , back, forward , reload , myWebView);
+
+		//VBox page = new VBox();
+		//root.getChildren().addAll(myWebView);
+		
+		Scene scene = new Scene(root, 1080, 720);
 		stage.setTitle("a quick brown fox jumps over the lazy fox");
 		stage.setScene(scene);
 		
