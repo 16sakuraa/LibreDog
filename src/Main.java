797e6e0549ba416f63546fe5442f9017276bc1e5
16sakuraa <64010860@kmitl.ac.th>
2022-05-02 14:42:12 +0700

    - Added Bookmarks - Bookmark page can be added by clicking on Star icon
    Known issue :
    - Bookmarks doesn't save after session is closed

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.scene.web.WebHistory;
@@ -21,6 +25,7 @@ public class Main extends Application {
 
 	private String homePage;
 	private WebHistory history;
+	private String bookmarkName;
 	TextField urlBar = new TextField();
 
 
@@ -30,19 +35,38 @@ public class Main extends Application {
 		WebEngine engine = myWebView.getEngine();
 		homePage = "https://www.google.com";
 		engine.load(homePage);
+		urlBar.setText(homePage);
 
 		urlBar.setOnAction(e -> {
 			engine.load("http://"+urlBar.getText());
 			});
-		
-	/*	Button btn = new Button("Load Page");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+		MenuButton bookmark = new MenuButton("Bookmarks");
+
+		Button fav = new Button("");
+		Image image = new Image(getClass().getResource("star.png").toExternalForm(), 20, 17, true, true);
+		fav.setGraphic(new ImageView(image));
+
+		fav.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
-			public void handle(ActionEvent event) {
-				engine.load("http://"+urlBar.getText());
+			public void handle(ActionEvent event) 
+			{
+				MenuItem bookmarkName = new MenuItem(urlBar.getText());
+				bookmark.getItems().add(bookmarkName);
+
+				bookmarkName.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						urlBar.setText(bookmarkName.getText());
+						engine.load("http://"+urlBar.getText());
+						
+					}
+				});
 			}
-		}); */
+			
+		});
+		
 		
 		Button back = new Button("Back");
 		back.setOnAction(new EventHandler<ActionEvent>() {
@@ -82,7 +106,7 @@ public class Main extends Application {
 		});
 
 		
-		engine.setUserAgent("Mozilla/5.0 (Linux; U; Android 4.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13");
+		engine.setUserAgent("Mozilla/99.0 (Linux; U; Android 7.0; en-us; Xoom Build/HRI39) AppleWebKit/534.13 (KHTML, like Gecko) Version/4.0 Safari/534.13");
 		//engine.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
 		
 		
@@ -92,15 +116,15 @@ public class Main extends Application {
 		VBox.setMargin(back , new Insets(0, 0, 10, 10));
 		VBox.setMargin(forward , new Insets(-35, 90, 10, 60));
 		VBox.setMargin(reload , new Insets(-35, 90, 10, 130));
-		
+		VBox.setMargin(bookmark , new Insets(-35 , 90 , 10 , 850));
+		VBox.setMargin(fav ,  new Insets(-35 , 90 , 10 , 800));
 
-		root.getChildren().addAll(urlBar , back, forward , reload , myWebView);
+		root.getChildren().addAll(urlBar , back, forward , reload , fav , bookmark , myWebView);
 
-		//VBox page = new VBox();
-		//root.getChildren().addAll(myWebView);
+	
 		
 		Scene scene = new Scene(root, 1080, 720);
-		stage.setTitle("a quick brown fox jumps over the lazy fox");
+		stage.setTitle("Firedog");
 		stage.setScene(scene);
 		
 		stage.show();
