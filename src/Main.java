cef3f6d179d0b18808e72d5e550094f2c7212c38
16sakuraa <64010860@kmitl.ac.th>
2022-05-02 15:35:58 +0700

    Major UI Changes

--- src/Main.java
+++ src/Main.java
@@ -25,41 +25,51 @@ public class Main extends Application {
 
 	private String homePage;
 	private WebHistory history;
-	private String bookmarkName;
-	TextField urlBar = new TextField();
+	//private String bookmarkName;
+	
 
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		WebView myWebView = new WebView();
 		WebEngine engine = myWebView.getEngine();
+
+		TextField urlBar = new TextField();
+		urlBar.setPrefWidth(300.00);
 		homePage = "https://www.google.com";
 		engine.load(homePage);
 		urlBar.setText(homePage);
+
+		Image bookmarkIcon = new Image(getClass().getResource("star.png").toExternalForm(), 20, 17, true, true);
+		Image backIcon = new Image(getClass().getResource("back.png").toExternalForm(), 20, 17, true, true);
+		Image fwIcon = new Image(getClass().getResource("forward.png").toExternalForm(), 20, 17, true, true);
+		Image reloadIcon = new Image(getClass().getResource("reload.png").toExternalForm(), 20, 17, true, true);
+
+		urlBar.textProperty().bind(engine.locationProperty()); // for detecting URL change - work perfectly but unable to type in url.
 
 		urlBar.setOnAction(e -> {
 			engine.load("http://"+urlBar.getText());
 			});
 
+
 		MenuButton bookmark = new MenuButton("Bookmarks");
-
 		Button fav = new Button("");
-		Image image = new Image(getClass().getResource("star.png").toExternalForm(), 20, 17, true, true);
-		fav.setGraphic(new ImageView(image));
-
+		
+		fav.setGraphic(new ImageView(bookmarkIcon));
 		fav.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) 
 			{
-				MenuItem bookmarkName = new MenuItem(urlBar.getText());
-				bookmark.getItems().add(bookmarkName);
+				MenuItem bookmarkName = new MenuItem(urlBar.getText()); 
+				bookmark.getItems().add(bookmarkName);					// add bookmark page to drop down menu
 
 				bookmarkName.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent event) {
+						
 						urlBar.setText(bookmarkName.getText());
-						engine.load("http://"+urlBar.getText());
+						engine.load("http://"+urlBar.getText());    // load bookmark page on click
 						
 					}
 				});
@@ -68,7 +78,8 @@ public class Main extends Application {
 		});
 		
 		
-		Button back = new Button("Back");
+		Button back = new Button("");
+		back.setGraphic(new ImageView(backIcon));
 		back.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -83,7 +94,8 @@ public class Main extends Application {
 			
 		});
 		
-		 Button forward = new Button("Forward");
+		 Button forward = new Button("");
+		 forward.setGraphic(new ImageView(fwIcon));
 		forward.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -96,7 +108,8 @@ public class Main extends Application {
 			}
 		}); 
 		
-		Button reload = new Button("Reload");
+		Button reload = new Button("");
+		reload.setGraphic(new ImageView(reloadIcon));
 		reload.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -112,18 +125,31 @@ public class Main extends Application {
 		
 		VBox root = new VBox();
 
-		VBox.setMargin(urlBar , new Insets(10, 10, 10, 10));
+		/*VBox.setMargin(urlBar , new Insets(10, 10, 10, 10));
 		VBox.setMargin(back , new Insets(0, 0, 10, 10));
-		VBox.setMargin(forward , new Insets(-35, 90, 10, 60));
+		VBox.setMargin(forward , new Insets(-35, 90, 10, 60));		// old button layout
 		VBox.setMargin(reload , new Insets(-35, 90, 10, 130));
 		VBox.setMargin(bookmark , new Insets(-35 , 90 , 10 , 850));
-		VBox.setMargin(fav ,  new Insets(-35 , 90 , 10 , 810));
+		VBox.setMargin(fav ,  new Insets(-35 , 90 , 10 , 810)); */
 
-		root.getChildren().addAll(urlBar , back, forward , reload , fav , bookmark , myWebView);
+
+
+		VBox.setMargin(urlBar , new Insets(-55, 30, 30, 130));
+		VBox.setMargin(back , new Insets(10, 10, 10, 10));
+		VBox.setMargin(forward , new Insets(-35, 20, 30, 50));		// new button layout 
+		VBox.setMargin(reload , new Insets(-55, 20, 30, 90));
+		VBox.setMargin(bookmark , new Insets(-35 , 90 , 10 , 50));
+		VBox.setMargin(fav ,  new Insets(-25 , 90 , 10 , 10));
+
+		
+
+		//root.getChildren().addAll(urlBar , back, forward , reload , fav , bookmark , myWebView);
+		root.getChildren().addAll(back, forward , reload , urlBar, fav , bookmark , myWebView);
 
 	
 		
 		Scene scene = new Scene(root, 1080, 720);
+		stage.getIcons().add(new Image("dog.png"));
 		stage.setTitle("Firedog");
 		stage.setScene(scene);
 		
